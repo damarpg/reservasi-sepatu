2c9dc9926be4060c0cc1f9a692ef5749062105cf
damarpg <damaralchantara12@gmail.com>
2025-12-17 06:41:46 +0700

    Delete tugas_akhir directory

--- tugas_akhir/generate_hash.php
+++
@@ -1,8 +0,0 @@
-<?php
-$password_plain = 'admin123';
-$hash = password_hash($password_plain, PASSWORD_DEFAULT);
-
-echo "Password yang digunakan: admin123\n";
-echo "Hash yang dihasilkan sistem Anda:\n";
-echo $hash;
-?>
